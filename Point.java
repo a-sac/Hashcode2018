a346c09d1fa8be68fb3f54c40876a42cc5c1c8c1
tiago <tiago.tdaa@gmail.com>
2018-03-01 18:28:37 +0000

    Added classes point and trip.

---
+++ Point.java
@@ -0,0 +1,26 @@
+public class Point{
+
+	private double x;
+	private double y;
+
+	public Point(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX(){
+		return this.x;
+	}
+
+	public double getY(){
+		return this.y;
+	}
+
+	public void setX(double xx){
+		this.x = xx;
+	}
+
+	public void setY(double yy){
+		this.y = yy;
+	}
+}
