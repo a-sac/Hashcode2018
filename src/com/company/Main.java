02242e36936dbc16303b3facc656c00d85171fb7
ZePedroResende <j.p.resende68@gmail.com>
2018-03-01 19:00:45 +0000

    adicionado muita cena

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,28 @@
 package com.company;
 
+import java.awt.*;
 import java.io.*;
 import java.util.Arrays;
+
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File file = new File("e_high_bonus.in");
+        File file = new File("a_example.in");
         BufferedReader input = new BufferedReader(new FileReader(file));
         String line;
+        Mapa map;
+        int numeros[];
+        line = input.readLine();
+        numeros = Arrays.asList(line.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+        map = new Mapa(numeros[0],numeros[1],numeros[2],numeros[3],numeros[4],numeros[5]);
+
+        System.out.println("\n");
         while ((line = input.readLine()) != null){
-            String split[]= line.split(" ");
-            Arrays.stream(split).forEach(System.out::println);
-            System.out.println("\n");
+            numeros = Arrays.asList(line.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+            Point p1 = new Point(numeros[0],numeros[1]);
+            Point p2 = new Point(numeros[2],numeros[3]);
+            Trip trip = new Trip(p1,p2,numeros[4],numeros[5]);
         }
     }
 }
